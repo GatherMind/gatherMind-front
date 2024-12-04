ec1d14c83a21265b96367e5f4045f829e70c0f90
rachael92724 <73717701+rachael92724@users.noreply.github.com>
2024-12-04 15:46:41 +0900

    add parameter, modify validateApiService

--- src/services/ValidateApiService.jsx
+++ src/services/ValidateApiService.jsx
@@ -18,23 +18,3 @@ export const duplicationCheck = async (field, value) => {
     throw new Error("Failed to fetch my Info.");
   }
 };
-
-// 이미지 업로드
-export const imageUpload = async (formData, token) => {
-  try {
-    const response = await axios.post(`${API_URL}/files/upload`, formData, 
-      { 
-        headers: {
-          'content-type': 'multipart/form-data',
-          Authorization: `Bearer ${token}`,
-        } 
-      }
-    );
-    console.log(response);
-
-    return response.data;
-  } catch (error) {
-    console.error("이미지 업로드 실패: ", error);
-    throw error;
-  }
-};
